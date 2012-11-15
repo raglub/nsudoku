84bc1f8e0f4d6ab44ba46f3d975e88faa92953f4
Michał Szyma <devszyma@gmail.com>
2012-11-15 23:55:52 +0100

    changed class name from Logic to Solver and improved tests

---
+++ lib/nsudoku/solver.rb
@@ -0,0 +1,160 @@
+# encoding: utf-8
+
+class NSudoku
+  class Solver
+
+    attr_accessor :revers
+
+    def initialize(sudoku)
+      @sudoku = sudoku
+      @width = Math.sqrt(sudoku.length).to_i
+      @block_width = Math.sqrt(@width).to_i
+      @revers = create_revers(@sudoku.split(""))
+    end
+
+    def search
+      revers = nil
+      while not revers == @revers
+        revers = Marshal.load(Marshal.dump(@revers))
+        @width.times do |index|
+          [:row, :column, :block].each do |type|
+            search_pair(type, index)
+            search_single(type, index)
+            only_one(type, index)
+          end
+        end
+      end
+      self
+    end
+
+    def create_revers(data)
+      @revers, index = raw_revers, 0
+      @revers.each_with_index do |row, row_index|
+        row.each_with_index do |col, col_index|
+          cell = data[index]
+          @revers[row_index][col_index] = [cell.to_i] if cell =~ /^[1-9]$/
+          index += 1
+        end
+      end
+    end
+
+    # revers table @revers_table into string where cell [n] return n and [a1, a2, ..., ak] return 0
+    def sudoku
+      result = ""
+      @revers.flatten(1).each do |element|
+        result << (element.size == 1 ? element.first.to_s : "0")
+      end
+      result
+    end
+
+    # change positions: external position of block for sudoku and
+    # internal position of block into array [row_position, column_position]
+    def block_position(external, internal)
+      [
+        (external / @block_width) * @block_width + internal / @block_width,
+        (external % @block_width) * @block_width + internal % @block_width
+      ]
+    end
+
+    # get vector values for row, column or block from variable @revers
+    def get_vector(type, index)
+      result = []
+      case type
+      when :row
+        result = @revers[index]
+      when :column
+        result = @revers.map{ |element| element[index] }
+      when :block
+        @width.times do |block|
+          row, column = block_position(index, block)
+          result << @revers[row][column]
+        end
+      end
+      result
+    end
+
+    # get vector values for row, column or block from variable @revers
+    def set_vector(type, index, vector)
+      case type
+      when :row
+        @revers[index] = vector
+      when :column
+        @width.times do |row|
+          @revers[row][index] = vector[row]
+        end
+      when :block
+        @width.times do |block|
+          row, column = block_position(index, block)
+          @revers[row][column] = vector[block]
+        end
+      end
+    end
+
+    def positions_values(vector)
+      result = {}
+      vector.each_with_index do |values, index|
+        values.each do |value|
+          (result[value] ||= []) << index
+        end
+      end
+      result
+    end
+
+    def only_two_positions(vector)
+      result = {}
+      only_two_positions = positions_values(vector).select {|k,v| v.length == 2}
+      only_two_positions.each { |value, positions| (result[positions] ||= []) << value }
+      result
+    end
+
+    # erase values in vertical, horizontal and block, which are that same like value from positnio row, column
+    def search_pair(type, index)
+      vec = get_vector(type, index)
+      only_two_positions(vec).each do |positions, value|
+        next unless value.size == 2
+        vec[positions[0]] = value.clone
+        vec[positions[1]] = value.clone
+      end
+      set_vector(type, index, vec)
+    end
+
+    # erase values in row, column and block which are that same like value from positnio row, column
+    def only_one(type, index)
+      vec = get_vector(type, index)
+      vec.each_with_index do |values, position|
+        next unless values.size == 1
+        cell = values.first
+        @width.times do |sub_position|
+          vec[sub_position].delete(cell) unless sub_position == position
+        end
+      end
+      set_vector(type, index, vec)
+    end
+
+    # erase values in vertical, which are that same like value from positnio row, column
+    def search_single(type, index)
+      vec = get_vector(type, index)
+      positions_values(vec).each do |value, positions|
+        vec[positions.first] = [value] if positions.size == 1
+      end
+      set_vector(type, index, vec)
+    end
+
+  private
+
+    # @width = 4
+    # cell = [1, 2, 3, 4]
+    # return  [[cell, cell, cell, cell],
+    #          [cell, cell, cell, cell],
+    #          [cell, cell, cell, cell],
+    #          [cell, cell, cell, cell]]
+    def raw_revers
+      (1..@width).to_a.map do
+        (1..@width).to_a.map do
+          (1..@width).to_a
+        end
+      end
+    end
+
+  end
+end
--- lib/nsudoku/version.rb
+++ lib/nsudoku/version.rb
@@ -1,3 +1,3 @@
 class NSudoku
-  VERSION = "0.1.6"
+  VERSION = "0.1.10"
 end
